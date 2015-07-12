4f597cfeb04a597d75dfd163decd818ff59f19c9
John Neijzen <john.neijzen@live.com>
2015-07-12 21:01:20 +0800

    Create 3MoveGame.c

---
+++ Programs/Game/3MoveGame.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // valuables for game
+    int PlayerOneHP = 100;
+    int PlayerTwoHP = 100;
+    int PlayerOneMove = 0;
+    int PlayerTwoMove = 0;
+    int PlayerOneWinCount = 0;
+    int PlayerTwoWinCount = 0;
+    int GameRounds = 0;
+    int MaxGameRounds = 0;
+    int Difficulty = 0;
+    int GameOn = 1;
+
+
+    // Basic Input From User
+    printf("\nHow many Rounds You Like: ");
+    scanf("%d",&MaxGameRounds);
+    printf("\nSelect Difficulty 1 = easy, 2 = mid, 3 = hard: ");
+    scanf("%d",&Difficulty);
+    printf("\nPress Any Key To Start");
+    getch();
+    system("cls");
+
+    while ( GameOn == 1 )
+    {
+        if (MaxGameRounds > GameRounds)
+        {
+            PlayerOneMove = 0;
+            PlayerTwoMove = 0;
+            // AI Player 2 Moves Chance of get hit is great as difficulty get higher
+            if (Difficulty == 1)
+            {
+                PlayerTwoMove = rand() % 4;
+            }
+            else if (Difficulty == 2)
+            {
+                PlayerTwoMove = rand() % 4;
+            }
+            else
+            {
+                PlayerTwoMove = rand() % 4;
+            }
+
+            // User Moves
+            printf("\nRound: %d", GameRounds);
+            printf("\n\nPlayer 1 wins: %d, Player 2 Wins: %d", PlayerOneWinCount, PlayerTwoWinCount);
+            printf("\n\nPlayer 1 HP: %d, Player 2 HP: %d", PlayerOneHP, PlayerTwoHP);
+            printf("\nNote Atk > Magic, Magic > Block, Block > Atk");
+            printf("\nPick a Move: 1 = Atk, 2 = Block, 3 = Magic. Move: ");
+            scanf("%d", &PlayerOneMove);
+
+            if ((PlayerOneMove == 1)&&(PlayerTwoMove == 1))
+            {
+                printf("\nBoth Player Used ATK So Both Player Get DMG");
+                PlayerOneHP = PlayerOneHP - 20;
+                PlayerTwoHP = PlayerTwoHP - 20;
+            }
+            else if((PlayerOneMove == 1)&&(PlayerTwoMove == 2))
+            {
+                printf("\nPlayer 2 Block ATK From Player 1");
+            }
+            else if((PlayerOneMove == 1)&&(PlayerTwoMove == 3))
+            {
+                printf("\nPlayer 1 ATK Player 2 Before Player 2 Can Use Magic");
+                PlayerTwoHP = PlayerTwoHP - 20;
+            }
+            else if((PlayerOneMove == 2)&&(PlayerTwoMove == 1))
+            {
+                printf("\nPlayer 1 Block ATK From Player 2");
+            }
+            else if((PlayerOneMove == 2)&&(PlayerTwoMove == 2))
+            {
+                printf("\nBoth Player Picked Block");
+            }
+            else if((PlayerOneMove == 2)&&(PlayerTwoMove == 3))
+            {
+                printf("\nBlock Cant Block Magic So Player 1 Get DMG");
+                PlayerOneHP = PlayerOneHP - 20;
+            }
+            else if((PlayerOneMove == 3)&&(PlayerTwoMove == 1))
+            {
+                printf("\nPlayer 2 ATK Player 1 Before Player 1 Can Use Magic");
+                PlayerOneHP = PlayerOneHP - 20;
+            }
+            else if((PlayerOneMove == 3)&&(PlayerTwoMove == 2))
+            {
+                printf("\nBlock Cant Block Magic So player 2 Get DMG");
+                PlayerTwoHP = PlayerTwoHP - 20;
+            }
+            else if((PlayerOneMove == 3)&&(PlayerTwoMove == 3))
+            {
+                printf("\nBoth Player Picked ATK So Both Player Get DMG");
+                PlayerOneHP = PlayerOneHP - 20;
+                PlayerTwoHP = PlayerTwoHP - 20;
+            }
+
+            // Not Needed It just Make Cleaner
+            getch();
+            system("cls");
+
+            if (PlayerOneHP == 0)
+            {
+                printf("\nPlayer Two Won Round");
+                GameRounds = GameRounds + 1;
+                // Reset Hp For Next Round
+                PlayerOneHP = 100;
+                PlayerTwoHP = 100;
+                PlayerTwoWinCount = PlayerTwoWinCount + 1;
+                getch();
+            }
+            else if (PlayerTwoHP == 0)
+            {
+                printf("\nPlayer One Won Round");
+                GameRounds = GameRounds + 1;
+                // Reset Hp For Next Round
+                PlayerOneHP = 100;
+                PlayerTwoHP = 100;
+                PlayerOneWinCount = PlayerOneWinCount + 1;
+                getch();
+            }
+        }
+        else
+        {
+            if (PlayerTwoWinCount > PlayerOneWinCount)
+            {
+                printf("\nPlayer Two Win Game");
+                GameOn = 0;
+            }
+            else if (PlayerOneWinCount > PlayerTwoWinCount)
+            {
+                printf("\nPlayer One Win Game");
+                GameOn = 0;
+            }
+            else
+            {
+                printf("\nBAKA! DRAW! NO ONE WON!");
+                GameOn = 0;
+            }
+        }
+    }
+    printf("\nThanks For Playing");
+    return 0;
+}
