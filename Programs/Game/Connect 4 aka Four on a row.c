3d503e47681e316d200c347fccb7f6cf35900794
John Neijzen <john.neijzen@live.com>
2015-09-07 21:40:17 +0800

    Update Connect 4 aka Four on a row.c

--- Programs/Game/Connect 4 aka Four on a row.c
+++ Programs/Game/Connect 4 aka Four on a row.c
@@ -1,256 +1,139 @@
 #include <stdio.h>
+#include <windows.h>
 
-// GameCreated By John V. Neijzen
-// Change-logs
-// 0.01 First Code 8/6/2015
-// 0.02 Adding More 8/7/2015
-// function declaration
+/*
+    Version
+        0.01
+    Change-logs
+        0.01 - complete rewrite of old one
+*/
 
-// Global Variables i looks weird
-int Player1Input = 0;
-int Player2Input = 0;
-int display[6][7] = {
-		0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0
+//Function
+void display();
+void gameLoop();
+void check();
+
+//Global Variables
+char grid[6][7] = {
+		'*', '*', '*', '*', '*', '*', '*',
+		'*', '*', '*', '*', '*', '*', '*',
+		'*', '*', '*', '*', '*', '*', '*',
+		'*', '*', '*', '*', '*', '*', '*',
+		'*', '*', '*', '*', '*', '*', '*',
+		'*', '*', '*', '*', '*', '*', '*'
 };
-int i,j,k;
-int column1[6] = {0, 0, 0, 0, 0, 0};
-int column2[6] = {0, 0, 0, 0, 0, 0};
-int column3[6] = {0, 0, 0, 0, 0, 0};
-int column4[6] = {0, 0, 0, 0, 0, 0};
-int column5[6] = {0, 0, 0, 0, 0, 0};
-int column6[6] = {0, 0, 0, 0, 0, 0};
-int column7[6] = {0, 0, 0, 0, 0, 0};
+int playerOneMove = 0;
+int playerTwoMove = 0;
+int column1 = 5;
+int column2 = 5;
+int column3 = 5;
+int column4 = 5;
+int column5 = 5;
+int column6 = 5;
+int column7 = 5;
 
-// Main Code
+
 int main()
 {
-	int Player = 1;
-	int WhoWon = 0;
+    int gameOn = 1;
 
-	Display();
-
-    while(WhoWon == 0)
+    display();
+    do
     {
-        if(Player == 1)
-        {
-            printf("Player 1 (1) Turn");
-            do
-            {
-                scanf("%d", &Player1Input);
-                if((Player1Input < 1)||(Player1Input > 7))
-                    printf("Try Aging");
-            }
-            while((Player1Input < 1)||(Player1Input > 7));
-            RunGameLoop();
-            Display();
-            Player1Input = 0;
-            Player = 2;
-        }
-        if(Player == 2)
-        {
-            printf("Player 2 (2) Turn");
-            do
-            {
-                scanf("%d", &Player2Input);
-                if((Player2Input < 1)||(Player2Input > 7))
-                    printf("Try Aging");
-            }
-            while((Player2Input < 1)||(Player2Input > 7));
-            RunGameLoop();
-            Display();
-            Player2Input = 0;
-            Player = 1;
-        }
+        printf("Player 1 move: ");
+        scanf("%d", &playerOneMove);
+        gameLoop();
+        playerOneMove = 0; // Reset back to normal
+        system("cls");
+        display();
+        check();
+        printf("Player 2 move: ");
+        scanf("%d", &playerTwoMove);
+        gameLoop();
+        playerTwoMove = 0; // Reset back to normal
+        system("cls");
+        display();
     }
-    return 0;
+    while(gameOn == 1);
 }
 
-// Display Code
-int Display()
+void display()
 {
-    system("cls");
+    int i,j;
 
-	// Display Print Out
-	printf("top of grid is coording");
-    printf("\n1 2 3 4 5 6 7\n");
-	for(i = 0; i < 6; i++)
+    printf("1 2 3 4 5 6 7\n");
+    for(i = 0; i < 6; i++)
 	{
 		for(j = 0; j < 7; j++)
 		{
-			printf("%d ", display[i][j]);
+			printf("%c ", grid[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-// Game Code
-int RunGameLoop()
+void gameLoop()
 {
-    // Player 1 Input
-    switch(Player1Input)
-    {
-        case 1:
-            column1[0] = 1;
-            break;
-        case 2:
-            column2[0] = 1;
-            break;
-        case 3:
-            column3[0] = 1;
-            break;
-        case 4:
-            column4[0] = 1;
-            break;
-        case 5:
-            column5[0] = 1;
-            break;
-        case 6:
-            column6[0] = 1;
-            break;
-        case 7:
-            column7[0] = 1;
-            break;
+    if(playerOneMove == 1){
+        grid[column1][0] = 'R';
+        column1 = column1 - 1;
+    }
+    else if(playerOneMove == 2){
+        grid[column2][1] = 'R';
+        column2 = column2 - 1;
+    }
+    else if(playerOneMove == 3){
+        grid[column3][2] = 'R';
+        column3 = column3 - 1;
+    }
+    else if(playerOneMove == 4){
+        grid[column4][3] = 'R';
+        column4 = column4 - 1;
+    }
+    else if(playerOneMove == 5) {
+        grid[column5][4] = 'R';
+        column5 = column5 - 1;
+    }
+    else if(playerOneMove == 6){
+        grid[column6][5] = 'R';
+        column6 = column6 - 1;
+    }
+    else if(playerOneMove == 7){
+        grid[column7][6] = 'R';
+        column7 = column7 - 1;
     }
 
-    // Player 2 Input
-    switch(Player2Input)
-    {
-        case 1:
-            column1[0] = 2;
-            break;
-        case 2:
-            column2[0] = 2;
-            break;
-        case 3:
-            column3[0] = 2;
-            break;
-        case 4:
-            column4[0] = 2;
-            break;
-        case 5:
-            column5[0] = 2;
-            break;
-        case 6:
-            column6[0] = 2;
-            break;
-        case 7:
-            column7[0] = 2;
-            break;
+    if(playerTwoMove == 1){
+        grid[column1][0] = 'Y';
+        column1 = column1 - 1;
     }
-
-    while((column1[0] == 1)||(column1[0] == 2))
-    {
-        for(i = 0; i < 6; i++)
-        {
-            if(column1[i] == 0)
-            {
-                column1[i] = column1[i-1];
-                column1[i-1] = 0;
-            }
-        }
+    else if(playerTwoMove == 2){
+        grid[column2][1] = 'Y';
+        column2 = column2 - 1;
     }
-
-    while((column2[0] == 1)||(column2[0] == 2))
-    {
-        for(i = 1; i < 6; i++)
-        {
-            if(column2[i] == 0)
-            {
-                column2[i] = column2[i-1];
-                column2[i-1] = 0;
-            }
-        }
+    else if(playerTwoMove == 3){
+        grid[column3][2] = 'Y';
+        column3 = column3 - 1;
     }
-
-    while((column3[0] == 1)||(column3[0] == 2))
-    {
-        for(i = 1; i < 6; i++)
-        {
-            if(column3[i] == 0)
-            {
-                column3[i] = column3[i-1];
-                column3[i-1] = 0;
-            }
-        }
+    else if(playerTwoMove == 4){
+        grid[column4][3] = 'Y';
+        column4 = column4 - 1;
     }
-
-    while((column4[0] == 1)||(column4[0] == 2))
-    {
-        for(i = 1; i < 6; i++)
-        {
-            if(column4[i] == 0)
-            {
-                column4[i] = column4[i-1];
-                column4[i-1] = 0;
-            }
-        }
+    else if(playerTwoMove == 5){
+        grid[column5][4] = 'Y';
+        column5 = column5 - 1;
     }
-
-    while((column5[0] == 1)||(column5[0] == 2))
-    {
-        for(i = 1; i < 6; i++)
-        {
-            if(column5[i] == 0)
-            {
-                column5[i] = column5[i-1];
-                column5[i-1] = 0;
-            }
-        }
+    else if(playerTwoMove == 6){
+        grid[column6][5] = 'Y';
+        column6 = column6 - 1;
     }
-
-    while((column6[0] == 1)||(column6[0] == 2))
-    {
-        for(i = 1; i < 6; i++)
-        {
-            if(column6[i] == 0)
-            {
-                column6[i] = column6[i-1];
-                column6[i-1] = 0;
-            }
-        }
+    else if(playerTwoMove == 7){
+        grid[column7][6] = 'Y';
+        column7 = column7 - 1;
     }
-
-    while((column7[0] == 1)||(column7[0] == 2))
-    {
-        for(i = 1; i < 6; i++)
-        {
-            if(column7[i] == 0)
-            {
-                column7[i] = column7[i-1];
-                column7[i-1] = 0;
-            }
-        }
-    }
-
-    for(i = 0; i < 7; i++)
-	{
-		for(j = 0; j < 6; j++)
-		{
-			if(i == 0)
-                display[j][i] = column1[j];
-            if(i == 1)
-                display[j][i] = column2[j];
-            if(i == 2)
-                display[j][i] = column3[j];
-            if(i == 3)
-                display[j][i] = column4[j];
-            if(i == 4)
-                display[j][i] = column5[j];
-            if(i == 5)
-                display[j][i] = column6[j];
-            if(i == 6)
-                display[j][i] = column7[j];
-		}
-	}
 }
 
-// Check Code
-int CheckCode()
+void check()
 {
-    
+
 }
