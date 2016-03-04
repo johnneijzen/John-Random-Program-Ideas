b608fa890355c3bc7cdc89e3042237299fb04f55
John Neijzen <john.neijzen@live.com>
2016-03-04 21:00:52 +0800

    Remove useless code and compacting and comments

--- Programs/Game/8 Queens C99 Main one.c
+++ Programs/Game/8 Queens C99 Main one.c
@@ -1,13 +1,14 @@
 /*
     @author:    John V. Neijzen
-    @activity:  Queen (8 x 8)
+    @activity:  Queen (C99)
     @section:   CSA 12 A
-    @version:   0.4
+    @version:   0.5
     @Change-logs:
         0.1 - First Draft
         0.2 - added Checker Function
         0.3 - adding Struct function like push
         0.4 - Fixed some bugs
+        0.5 - Change row and col to use stack not display
 */
 
 
@@ -27,7 +28,7 @@ int solve(int firstQueenRow, int FirstQueenCol,int N,int (*displayGird)[N]);
 void push(int posRow, int posCol);
 void pop();
 void printStack();
-int checker(int row, int col, int nQueens,int N,int (*displayGird)[N]);
+int checker(int row, int col, int nQueens);
 
 
 int main()
@@ -65,22 +66,20 @@ int main()
 int solve(int firstQueenRow, int FirstQueenCol,int N,int (*displayGird)[N])
 {
     int checked = 0, nQueens = 1;
-    int row = 0,col = 0;
-    int row1 = 0, col1 = 0;
-    int counter = 0;
+    int row = 0,col = 0; // Loop value
+    int row1 = 0, col1 = 0; // This is used for when backtracking aka get pos from last queen in stack.
 
     struct QueenStack *temp;
 
     while(nQueens < N)
     {
-
         for(col=col1;col<N;col++)
         {
-            if(firstQueenRow == row)
+            if(firstQueenRow == row) // First Queen is one same row move row down by 1
             {
                 row++;
             }
-            checked = checker(row,col,nQueens,N,displayGird);
+            checked = checker(row,col,nQueens);
             if(checked == 1)
             {
                 push(row,col);
@@ -89,10 +88,17 @@ int solve(int firstQueenRow, int FirstQueenCol,int N,int (*displayGird)[N])
                 display(N,displayGird);
                 nQueens = nQueens + 1;
                 row++;
-                col1 = 0;
+                col1 = 0; // After checked 1 and queen has been place row goes down by 1 and col1 resets.
                 break;
             }
         }
+        /*
+            This Below is backtracking part
+
+            it used stacks to get pos of last queen then remove from display and delete from stack and
+            set row and col+1 from last queen from stack back into loop and for loop goes back to start.
+            but if pop so many times that reach to first queen then it will no solution.
+        */
 
         if(checked == 0)
         {
@@ -117,6 +123,7 @@ int solve(int firstQueenRow, int FirstQueenCol,int N,int (*displayGird)[N])
 			nQueens--;
 		}
     }
+    return 0;
 }
 
 void push(int posRow, int posCol) // This will always add at end
@@ -158,18 +165,7 @@ void pop()
     free(temp);
 }
 
-void printStack() // This is just temp it be remove later on
-{
-    struct QueenStack *temp;
-    temp = head;
-    while (temp != NULL)
-    {
-        printf("row = %d, col = %d \n", temp->posRow, temp->posCol);
-        temp = temp->next;
-    }
-}
-
-int checker(int row, int col, int nQueens,int N,int (*displayGird)[N])
+int checker(int row, int col, int nQueens)
 {
     /*
         There are 3 ways queen can atk
@@ -182,25 +178,19 @@ int checker(int row, int col, int nQueens,int N,int (*displayGird)[N])
     */
     struct QueenStack *temp;
     temp = head;
-    int diagonal,ctr,ctr1;
+    int ctr;
 
-    // Check Row and CoL
-    for(ctr=0;ctr<N;ctr++)
-    {
-        if((displayGird[row][ctr]=='Q')||(displayGird[ctr][col]=='Q'))
-        {
-            return 0;
-        }
-    }
-
-    // Check diagonal
     for(ctr = 0;ctr < nQueens; ctr++)
     {
         int row2,col2;
         row2 = temp->posRow;
         col2 = temp->posCol;
         temp = temp->next;
-        if(abs(row - row2)==abs(col - col2))
+        if(abs(row - row2)==abs(col - col2)) // Check diagonal
+        {
+            return 0;
+        }
+        else if((row == row2)||(col == col2)) // Check Rows and Cols
         {
             return 0;
         }
