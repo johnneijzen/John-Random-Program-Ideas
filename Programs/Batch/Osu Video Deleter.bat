6bb4f9cebec5616a763574db2e7a5b92d3d28dce
John Neijzen <john.neijzen@live.com>
2017-05-30 05:39:26 +0800

    Update Osu Video Deleter.bat

--- Programs/Batch/Osu Video Deleter.bat
+++ Programs/Batch/Osu Video Deleter.bat
@@ -2,7 +2,6 @@
 set /p pathName=Enter Path to Folder where you want delete video from?
 cd /d %pathName%
 set /p check=is this correct path to your osu song folder: %pathName%?
-@echo on
 IF %check%==y (
 	echo okey press any key to start program
 	pause
@@ -14,5 +13,6 @@ IF %check%==y (
 	echo Restart program
 	pause
 )
+@echo on
 echo Thank You Using John Osu Video Deleter Program press any key to quit
 pause
