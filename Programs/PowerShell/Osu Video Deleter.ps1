e9fa07e0b7dccd84b07cf3c2c673398fcf57df8d
John Neijzen <john.neijzen@live.com>
2017-10-18 03:41:02 +0800

    Create Osu Video Deleter.ps1

---
+++ Programs/PowerShell/Osu Video Deleter.ps1
@@ -0,0 +1,15 @@
+$path = Read-Host "Please enter path to osu song folder"
+$check = Read-Host "is this correct path to your osu song folder: $path"
+if($check -eq "y")
+{
+    Get-ChildItem -path $path -include *.avi -Recurse | Remove-Item
+    Get-ChildItem -path $path -include *.flv -Recurse | Remove-Item
+    Get-ChildItem -path $path -include *.wmv -Recurse | Remove-Item
+    Get-ChildItem -path $path -include *.mp4 -Recurse | Remove-Item
+}
+else
+{
+    Write-Host "Restart Program"
+}
+Write-Host "Thanks for using OSU! Video Deleter"
+	
